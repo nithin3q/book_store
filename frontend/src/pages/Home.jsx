7c4516dbac79536bd81441753a7b54ef64a4dfb6
Nithin Kumar Appari <52022957+nithin3q@users.noreply.github.com>
2024-01-27 09:20:55 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5553/books")
+      .get("https://book-store-five-rose.vercel.app/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
